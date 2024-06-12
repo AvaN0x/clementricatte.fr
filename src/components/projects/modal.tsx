9e7b9e5f8a74df30c75799bfdd2060d74fbfd8a1
AvaN0x <clement.ricatte57@gmail.com>
2024-06-12 18:01:26 +0200

    Revert "fix: ⚗️ try to use vaul directly for the ios issue"
    This reverts commit 536fbcd22e267067d28151a626004a00a59758fc.

--- src/components/projects/modal.tsx
+++ src/components/projects/modal.tsx
@@ -4,6 +4,16 @@ import * as React from "react";
 import type { Project } from "@/components/projects/type";
 import { cn } from "@/lib/utils";
 import { TechList } from "@/components/techs";
+import {
+  Drawer,
+  DrawerClose,
+  DrawerContent,
+  DrawerDescription,
+  DrawerFooter,
+  DrawerHeader,
+  DrawerTitle,
+  DrawerTrigger,
+} from "@/components/ui/drawer";
 import { Button } from "@/components/ui/button";
 import { IconX } from "@/components/icons";
 import { ProjectLinks } from "@/components/projects/links";
@@ -16,8 +26,6 @@ import {
   DialogTitle,
   DialogTrigger,
 } from "@/components/ui/dialog";
-import { Drawer } from "vaul";
-import { DrawerFooter, DrawerHeader } from "@/components/ui/drawer";
 
 const modalTriggerClass =
   "size-full text-left rounded-lg focus-visible-ring focus-visible:ring-inset focus-visible:ring-offset-0";
@@ -75,49 +83,48 @@ export function ProjectModal({
   }
 
   return (
-    <Drawer.Root>
-      <Drawer.Trigger className="size-full">{children}</Drawer.Trigger>
-      <Drawer.Portal>
-        <Drawer.Overlay className="fixed inset-0 bg-black/40" />
-        <Drawer.Content className="bg-background flex flex-col rounded-t-[10px] mt-24 fixed bottom-0 left-0 right-0">
-          <DrawerHeader
-            className={cn(
-              project.modal &&
-                "relative after:content-[''] after:absolute after:bottom-0 after:inset-x-8 after:mb-2 after:h-[1px] after:bg-border/50"
-            )}
-          >
-            <div className="flex w-full mb-2">
-              <Drawer.Title className="flex-1 self-center">
-                {project.name && <>{project.name} · </>}
-                {project.title}
-              </Drawer.Title>
-
-              <Drawer.Close asChild>
-                <Button variant="outline" className="self-start p-2 h-fit">
-                  <IconX />
-                </Button>
-              </Drawer.Close>
-            </div>
-            {"education" in project && (
-              <Drawer.Description className="text-foreground">
-                {project.education}
-              </Drawer.Description>
-            )}
-            {project.description && (
-              <Drawer.Description>{project.description}</Drawer.Description>
-            )}
-            <div className="flex w-full justify-between items-center flex-wrap min-[450px]:flex-nowrap">
-              <TechList techs={project.techs} />
-              <ProjectLinks links={project.links} />
-            </div>
-          </DrawerHeader>
-          {project.modal && (
-            <DrawerFooter className="overflow-y-auto text-justify">
-              {project.modal}
-            </DrawerFooter>
+    <Drawer open={open} onOpenChange={setOpen}>
+      <DrawerTrigger className={cn(modalTriggerClass, className)}>
+        {children}
+      </DrawerTrigger>
+      <DrawerContent className="max-w-screen-lg mx-auto max-h-[85vh] pb-10">
+        <DrawerHeader
+          className={cn(
+            project.modal &&
+              "relative after:content-[''] after:absolute after:bottom-0 after:inset-x-8 after:mb-2 after:h-[1px] after:bg-border/50"
           )}
-        </Drawer.Content>
-      </Drawer.Portal>
-    </Drawer.Root>
+        >
+          <div className="flex w-full mb-2">
+            <DrawerTitle className="flex-1 self-center">
+              {project.name && <>{project.name} · </>}
+              {project.title}
+            </DrawerTitle>
+
+            <DrawerClose asChild>
+              <Button variant="outline" className="self-start p-2 h-fit">
+                <IconX />
+              </Button>
+            </DrawerClose>
+          </div>
+          {"education" in project && (
+            <DrawerDescription className="text-foreground">
+              {project.education}
+            </DrawerDescription>
+          )}
+          {project.description && (
+            <DrawerDescription>{project.description}</DrawerDescription>
+          )}
+          <div className="flex w-full justify-between items-center flex-wrap min-[450px]:flex-nowrap">
+            <TechList techs={project.techs} />
+            <ProjectLinks links={project.links} />
+          </div>
+        </DrawerHeader>
+        {project.modal && (
+          <DrawerFooter className="overflow-y-auto text-justify">
+            {project.modal}
+          </DrawerFooter>
+        )}
+      </DrawerContent>
+    </Drawer>
   );
 }
